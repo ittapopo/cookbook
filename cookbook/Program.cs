039906ea37d0e455fee48324dcbe4edf9b34102c
Ittapopo <stianstian95gmail.com>
2020-04-03 12:29:09 +0200

    workin a bit

--- cookbook/Program.cs
+++ cookbook/Program.cs
@@ -7,23 +7,21 @@ using static System.Console;
 
 namespace cookbook
 {
-    class Program
+    public class Chapter1
     {
         static void Main(string[] args)
         {
             int[] scores = { 17, 46, 39, 62, 81, 79, 52, 24 };
             Chapter1 ch1 = new Chapter1();
-            var s = ch1.GetAverageAndCount(scores);
-            WriteLine($"Average was {s.average} across {s.studentCount} students");
+            var (average, studentCount) = ch1.GetAverageAndCount(scores);
+            WriteLine($"Average was {average} across {studentCount} students");
             ReadLine();
         }
-    }
 
-    public class Chapter1
-    {
         public (int average, int studentCount) GetAverageAndCount(int[] scores)
         {
             var returnTuple = (ave:0, sCount:0);
+            returnTuple = (scores.Sum()/scores.Count(), scores.Count());
             return returnTuple;
         }
     }
