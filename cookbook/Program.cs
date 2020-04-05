980be4a7e3e0757e341ae74ed966c981b950bbc3
Ittapopo <stianstian95gmail.com>
2020-04-05 13:42:12 +0200

    looking at pattern matching

--- cookbook/Program.cs
+++ cookbook/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using static System.Console;
 using static System.Math;
+using System.Collections.Generic;
 
 namespace cookbook
 {
@@ -12,11 +13,31 @@ namespace cookbook
     {
         static void Main(string[] args)
         {
+            //      Playing with tuples
+            /*** 
             int[] scores = { 17, 46, 39, 62, 81, 79, 52, 24, 49 };
             Chapter1 ch1 = new Chapter1();
             int threshold = 40;
             var (average, studentCount, belowAverage) = ch1.GetAverageAndCount(scores, threshold);
             WriteLine($"Average was {Round(average,2)} across {studentCount} students. { (average < threshold ? " Class score below average." : " Class score above average.")}");
+            ReadLine();
+                        */
+
+            Chapter1 ch1 = new Chapter1();
+
+            Student student = new Student();
+            student.Name = "Dirk";
+            student.LastName = "Strauss";
+            student.CourseCodes = new List<int> {  203, 202, 101 };
+
+            ch1.OutputInformation(student);
+
+            Professor prof = new Professor();
+            prof.Name = "Reinhardt";
+            prof.LastName = "Botha";
+            prof.TeachesSubjects = new List<string> { "Mobile Development", "Cryptography" };
+
+            ch1.OutputInformation(prof);
             ReadLine();
         }
 
@@ -26,6 +47,33 @@ namespace cookbook
             returnTuple = ((double)scores.Sum() / scores.Count(), scores.Count(), 
             returnTuple.ave.CheckIfBelowAverage(threshold));
             return returnTuple;
+        }
+
+        public void OutputInformation(object person)
+        {
+            if (person is Student student)
+            {
+                WriteLine($"Student {student.Name} {student.LastName} is enrolled for courses {String.Join<int>(",", student.CourseCodes)}");
+            }
+
+            if (person is Professor prof)
+            {
+                WriteLine($"Professor {prof.Name} {prof.LastName} teaches {String.Join<string>(",", prof.TeachesSubjects)}");
+            }
+        }
+
+        public class Student
+        {
+            public string Name { get; set; }
+            public string LastName { get; set; }
+            public List<int> CourseCodes { get; set; }
+        }
+
+        public class Professor
+        {
+            public string Name { get; set; }
+            public string LastName { get; set; }
+            public List<string> TeachesSubjects { get; set; }
         }
     }
 
